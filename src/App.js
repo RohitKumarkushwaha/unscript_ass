30ba1a31ce6cffafe12c0519e147f6d724d85a3a
RohitKumarKushwaha <rohit100mit@gmail.com>
2024-10-23 00:23:46 +0530

    unscript

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/HeroSection";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+
+const App=()=>{
+    return(
+      <div>
+      <Navbar/>
+      <Hero/>
+      <Features/>
+      <Footer/>
+      </div>
+    );
+}
+
+
+export default App;
